6ed40e3ca8a63727d7d2ad646d393163e7c9d1af
dogukanakkaya <doguakkaya27@hotmail.com>
2023-09-04 00:31:12 +0300

    chore: check light mode styling

--- components/Proposals/Time.tsx
+++ components/Proposals/Time.tsx
@@ -3,7 +3,7 @@ import { timeToHumanReadable } from "@/utils/string.ts";
 export default function Time({ startAt, endAt }: { startAt: number; endAt: number }) {
   if (Date.now() > endAt) {
     return (
-      <span class="flex items-center justify-center w-1/3 h-16 bg-gray-900 hover:bg-opacity-50 rounded-xl cursor-pointer">
+      <span class="flex items-center justify-center w-1/3 h-16 bg-gray-200 dark:bg-gray-900 hover:bg-opacity-50 rounded-xl cursor-pointer">
         {timeToHumanReadable(endAt)} ago <i class="bi bi-clock-history ml-2"></i>
       </span>
     );
@@ -11,14 +11,14 @@ export default function Time({ startAt, endAt }: { startAt: number; endAt: numbe
 
   if (Date.now() > startAt && Date.now() < endAt) {
     return (
-      <span class="flex items-center justify-center w-1/3 h-16 bg-gray-900 hover:bg-opacity-50 rounded-xl cursor-pointer">
+      <span class="flex items-center justify-center w-1/3 h-16 bg-gray-200 dark:bg-gray-900 hover:bg-opacity-50 rounded-xl cursor-pointer">
         {timeToHumanReadable(endAt)} left <i class="bi bi-hourglass-split ml-2"></i>
       </span>
     );
   }
 
   return (
-    <span class="flex items-center justify-center w-1/3 h-16 bg-gray-900 hover:bg-opacity-50 rounded-xl cursor-pointer">
+    <span class="flex items-center justify-center w-1/3 h-16 bg-gray-200 dark:bg-gray-900 hover:bg-opacity-50 rounded-xl cursor-pointer">
       in {timeToHumanReadable(startAt)} <i class="bi bi-alarm ml-2"></i>
     </span>
   );
