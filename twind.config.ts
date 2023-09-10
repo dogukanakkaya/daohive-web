6f1c191a456ad2e42860cd2d964ee144e192a510
dogukanakkaya <doguakkaya27@hotmail.com>
2023-09-10 21:28:15 +0300

    chore: scroll padding top for anchors caused by sticky header

--- twind.config.ts
+++ twind.config.ts
@@ -1,21 +1,27 @@
 import { Options } from "$fresh/plugins/twind.ts";
-import { apply } from "twind";
+import { apply, CSSRules } from "twind";
 import { gray } from "twind/colors";
 
 export default {
   selfURL: import.meta.url,
-  preflight: {
+  preflight: (preflight, { theme }) => ({
+    ...preflight,
     "@import": [
       "url('https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;500;600;700&display=swap')",
       "url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css')",
     ],
     ":root": {
       "--bg-header": gray["50"],
+      ...preflight[":root"] as CSSRules,
     },
     "@media (prefers-color-scheme: dark)": {
       ":root": {
         "--bg-header": "#040a15",
       },
+    },
+    html: {
+      scrollPaddingTop: theme("spacing.24"),
+      ...preflight["html"] as CSSRules,
     },
     body: apply`text-gray-600 dark:text-gray-300 bg-gray-100 dark:bg-[#070d19]`,
     ".nav-link": apply`block font-semibold text-lg px-4 py-2 transition hover:text-primary`,
@@ -26,7 +32,7 @@ export default {
     "#mobile-menu:checked ~ [for=mobile-menu] span:nth-child(1)": apply`-rotate-45 translate-y-2.5`,
     "#mobile-menu:checked ~ [for=mobile-menu] span:nth-child(2)": apply`rotate-45`,
     "#mobile-menu:checked ~ [for=mobile-menu] span:nth-child(3)": apply`hidden`,
-  },
+  }),
   theme: {
     fontFamily: {
       sans: ["Nunito", "sans-serif"],
