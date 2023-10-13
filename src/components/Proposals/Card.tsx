396cd6f72c3b56c940cd6066bf0c3932fa4f722f
dogukanakkaya <doguakkaya27@hotmail.com>
2023-10-13 23:58:36 +0300

    chore: remove static restriction & add ... for long desc

--- src/components/Proposals/Card.tsx
+++ src/components/Proposals/Card.tsx
@@ -11,12 +11,12 @@ export default function Card({ proposal }: { proposal: Proposal }) {
         <h1>
           <a href={`/${proposal.address}/${proposal.id}?type=${proposal.type}`} className="text-2xl dark:text-white font-semibold">{proposal.metadata.name}</a>
         </h1>
-        <p className="mt-2 dark:text-[#8bacda]">{proposal.metadata.description}</p>
+        <p className="mt-2 dark:text-[#8bacda]">{proposal.metadata.description.length > 100 ? `${proposal.metadata.description.slice(0, 100)}...` : proposal.metadata.description}</p>
       </div>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4 w-full">
           <span className="flex items-center justify-center w-1/2 h-16 bg-gray-200 dark:bg-gray-900 hover:bg-opacity-50 rounded-xl cursor-pointer">
-            Private <i className="bi bi-shield-check ml-2"></i>
+            {proposal.type === 'VotingPrivate' ? 'Private' : 'Public'} <i className="bi bi-shield-check ml-2"></i>
           </span>
           <Time className="w-1/2 h-16 cursor-pointer" startAt={proposal.startAt * 1000} endAt={proposal.endAt * 1000} />
         </div>
